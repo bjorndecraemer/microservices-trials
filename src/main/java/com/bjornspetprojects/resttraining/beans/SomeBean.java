0cca9d35aeb8baba88998b2940c2439ac0ac5ffb
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-25 00:49:42 +0100

    Some static filtering

---
+++ src/main/java/com/bjornspetprojects/resttraining/beans/SomeBean.java
@@ -0,0 +1,23 @@
+package com.bjornspetprojects.resttraining.beans;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class SomeBean {
+
+    private String value1;
+    @JsonIgnore
+    private String value2;
+    private String value3;
+
+    public SomeBean(String value1, String value2, String value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+}
