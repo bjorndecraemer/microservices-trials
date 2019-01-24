5fc165f08431b5771f4d8ab5e255fac63a25d550
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-24 21:04:14 +0100

    intermediate commit

--- src/main/java/com/bjornspetprojects/resttraining/ResttrainingApplication.java
+++ src/main/java/com/bjornspetprojects/resttraining/ResttrainingApplication.java
@@ -2,6 +2,13 @@ package com.bjornspetprojects.resttraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class ResttrainingApplication {
@@ -10,5 +17,11 @@ public class ResttrainingApplication {
         SpringApplication.run(ResttrainingApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver(){
+        AcceptHeaderLocaleResolver sLR = new AcceptHeaderLocaleResolver();
+        sLR.setDefaultLocale(Locale.ENGLISH);
+        return sLR;
+    }
 }
 
--- src/main/java/com/bjornspetprojects/resttraining/controllers/UserController.java
+++ src/main/java/com/bjornspetprojects/resttraining/controllers/UserController.java
@@ -3,9 +3,9 @@ package com.bjornspetprojects.resttraining.controllers;
 import com.bjornspetprojects.resttraining.beans.User;
 import com.bjornspetprojects.resttraining.exception.UserNotFoundException;
 import com.bjornspetprojects.resttraining.services.UserDaoService;
-import org.springframework.hateoas.Link;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -20,9 +21,11 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RestController
 public class UserController {
 
+    private final MessageSource messageSource;
     private final UserDaoService userDaoService;
 
-    public UserController(UserDaoService userDaoService) {
+    public UserController(MessageSource messageSource, UserDaoService userDaoService) {
+        this.messageSource = messageSource;
         this.userDaoService = userDaoService;
     }
 
@@ -34,14 +37,14 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<Object> findOne(@PathVariable Long id)throws UserNotFoundException{
+    public Resource<User> findOne(@PathVariable Long id)throws UserNotFoundException{
         User foundUser = userDaoService.findOne(id);
         if(foundUser == null) {
-            throw new UserNotFoundException("id - "+id);
+            throw new UserNotFoundException(messageSource.getMessage("error.user.not.found",null, LocaleContextHolder.getLocale())+"id - "+id);
         }
         Resource<User> resource = new Resource<>(foundUser);
         resource.add(linkTo(methodOn(this.getClass()).findAll()).withRel("all-users"));
-        return ResponseEntity.ok().body(resource);
+        return resource;
     }
 
     @DeleteMapping("/users/{id}")
